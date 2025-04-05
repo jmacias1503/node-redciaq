55cce8d01856f13e2144a0116c93e337aed8dc3d
Alejandro Macias <alex.mac.fon@gmail.com>
2025-04-05 17:48:55 -0600

    feat: added connection to container

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import mysql.connector
+
+conn = mysql.connector.connect(
+        host="weewx",
+        port=3306,
+        user="weewx",
+        password="weewx"
+        )
+
+conn.close()
